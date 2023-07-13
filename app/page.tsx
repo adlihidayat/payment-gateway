7000ef30c024d992d2cbe990109a8a7672f30ff3
oneiric 30 <adlihidayat30@gmail.com>
2023-07-13 18:40:28 +0700

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,14 @@ import { checkout } from "../checkout";
 export default function Home() {
   return (
     <main className="">
-      <h2 className=" text-2xl">buy below</h2>
+      <h2 className=" text-2xl">buy below fast</h2>
       <button
         className=" bg-slate-200 p-1 border border-black hover:bg-slate-100"
         onClick={() => {
           checkout({
             lineItems: [
               {
-                price: "price_1NTHrMFNdU6DayXcwfbfOCC4",
+                price: "price_1NTHrMFNdU6DayXcwfbfOCC4 ",
                 quantity: 1,
               },
             ],
