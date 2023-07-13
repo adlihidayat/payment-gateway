c3fe781d4a3a85c3709a779ab9c7e59d00f6a19e
oneiric 30 <adlihidayat30@gmail.com>
2023-07-13 18:27:20 +0700

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,24 @@
-import Image from "next/image";
+"use client";
+import { checkout } from "../checkout";
 
 export default function Home() {
   return (
     <main className="">
       <h2 className=" text-2xl">buy below</h2>
-      <button className=" bg-slate-200 p-1 border border-black hover:bg-slate-100">
-        click me!
+      <button
+        className=" bg-slate-200 p-1 border border-black hover:bg-slate-100"
+        onClick={() => {
+          checkout({
+            lineItems: [
+              {
+                price: "price_1NTHrMFNdU6DayXcwfbfOCC4",
+                quantity: 1,
+              },
+            ],
+          });
+        }}
+      >
+        BUY!
       </button>
     </main>
   );
